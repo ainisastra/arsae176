816f1dfcd9a29eaf6b6c9442a372ca1e4cf49a4a
Aini Sastra <aini.sastra@gmail.com>
2022-08-09 18:33:03 +0800

    Add files via upload

---
+++ wiringview.js
@@ -0,0 +1,3 @@
+var ars = 'https://www.wiringview.co';
+
+if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
