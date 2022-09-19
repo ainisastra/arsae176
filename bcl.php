6fe566669832b7485c386b8507da338f12fa9244
Aini Sastra <aini.sastra@gmail.com>
2022-09-19 23:31:49 +0800

    Add files via upload

---
+++ bcl.php
@@ -0,0 +1,9 @@
+<?php
+$randomdomain = array();
+$randomdomain[] = 'www.caretxdigital.com';
+$randomdomain[] = 'www.176iot.com';
+$randomdomain[] = 'www.wiringdraw.com';
+$randomdomain[] = 'www.panoramaline.com';
+$randomdomain[] = 'www.wiringstech.com';
+shuffle($randomdomain);
+?>
