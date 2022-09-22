2c296c51fc91e7cc9893e51f745e3a04034da3d6
Aini Sastra <aini.sastra@gmail.com>
2022-09-22 10:22:10 +0800

    Delete bcl.php

--- bcl.php
+++
@@ -1,9 +0,0 @@
-<?php
-$randomdomain = array();
-$randomdomain[] = 'www.caretxdigital.com';
-$randomdomain[] = 'www.176iot.com';
-$randomdomain[] = 'www.wiringdraw.com';
-$randomdomain[] = 'www.panoramaline.com';
-$randomdomain[] = 'www.wiringstech.com';
-shuffle($randomdomain);
-?>
